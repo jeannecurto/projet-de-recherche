0210bd6a6c3a3b165b94ecfef1fa4f81a577c734
jeannecurto <241156@umons.ac.be>
2026-05-08 10:31:01 +0000

    début

---
+++ exploration.R
@@ -0,0 +1,16 @@
+#Nous allons avoir un excel avec les résultats de séquençage de 19 échantillons (parfois 500 séquences, parfois 300 séquences détectées)
+
+#Question biologique 1
+#Quelles sont les stations qui se ressemblent le plus et lesquels sont les plus différentes
+#--> ACP
+
+#Question biologique 2
+#Quelles sont les 10 genres bactériens les plus abondants ? Parmi ces 10 genres quels sont les plus corrélés avec les conditions environnementales ? Prendre un genre (par ex : pseudomonas) et voir avec quelles conditions c'est corrélés (par ex : le pH ou la température)
+
+#Il est important de se focaliser sur les genres les plus connus
+
+#Question biologiques 3
+#Est-ce qu'une bactérie est plus significativement présentes qu'une autre ?
+
+#Le but est quand-même de voir si on a des genres bactériens qui sont en lien avec la condition dans laquelle ils se trouvaient
+
