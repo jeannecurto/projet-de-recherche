528225775b624a0e64923ef22112f27b4698534f
jeannecurto <241156@umons.ac.be>
2026-05-13 07:59:21 +0000

    changements

--- échantillon7.R
+++ échantillon7.R
@@ -7,6 +7,17 @@ data %>.%
   filter(., genus != "Unknown") %>.%
   pivot_longer(., cols = -genus, names_to = "station", values_to = "abondance") -> data_16
 
+top20_genres_16 <- data_16 %>%
+  group_by(genus) %>%
+  summarise(total_abondance = sum(abondance, na.rm = TRUE)) %>%
+  arrange(desc(total_abondance)) %>%
+  slice_head(n = 20) %>%
+  pull(genus)
+
+top20_data_16 <- data_16 %>%
+  filter(genus %in% top20_genres_16) %>%
+  mutate(genus = factor(genus, levels = top20_genres_16))
+
 top20_data_16_col <- top20_data_16 %>%
   mutate(type = case_when(
     genus == "Escherichia" ~ "E.coli",
@@ -22,7 +33,8 @@ chart(data=top20_data_16_col, abondance~ genus %fill=% type)+
     "Coliformes" = "#E78AC3",
     "Non-coliformes" = "#d9d9d9"
   )) +
-  labs(fill = "Type bactérien")
+  labs(fill = "Type bactérien")+
+  theme(axis.title.x=element_blank())
 
 data%>.%
   select(., "genus", "barcode16","family")%>.%
@@ -32,17 +44,17 @@ filter(., genus != "Unknown", family=="Enterobacteriaceae") %>.%
 data_entero %>%
 mutate(type = case_when(
   genus == "Escherichia" ~ "E.coli",
-  genus %in% c("Klebsiella","Citrobacter","Enterobacter","Raoultella","Cronobacter","Kluyvera", "Lelliottia","Pseudocitrobacter") ~ "Coliformes",
-  TRUE ~ "Non-coliformes"
+  TRUE ~ "Coliformes"
 ))%>%
-arrange(desc(abondance))-> data_entero_order
+arrange(desc(abondance))%>%
+mutate(genus = factor(genus, levels = genus))-> data_entero_order
 
-chart(data=data_entero_m, abondance~ genus %fill=% type)+
+chart(data=data_entero_order, abondance ~ fct_reorder(genus, abondance, .desc=TRUE) %fill=% type)+
   geom_col()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))+
   scale_fill_manual(values = c(
     "E.coli" = "#46ACBE",
-    "Coliformes" = "#E78AC3",
-    "Non-coliformes" = "#d9d9d9"
+    "Coliformes" = "#E78AC3"
   )) +
-  labs(fill = "Type bactérien")
+  labs(fill = "Type bactérien")+
+  theme(axis.title.x=element_blank())
