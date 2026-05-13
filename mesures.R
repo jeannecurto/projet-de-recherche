a56be2f06533b07d60c1a82907fccb0cd0ba5fa0
jeannecurto <241156@umons.ac.be>
2026-05-13 14:50:17 +0000

    ?

--- mesures.R
+++ mesures.R
@@ -32,4 +32,57 @@ rownames_to_column(new, var="station")->lol
 
 left_join(data_small, lol, by="station")-> full_data
 
+data_small%>.%
+arrange(., desc(abondance)) %>.%
+  filter(., genus != "Unknown") %>.%
+  slice_head(., n = 50)-> mini_full_data
 
+# analyses
+
+# =========================
+# CORRELATIONS + HEATMAP
+# =========================
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tibble)
+
+# 1. Renommer les variables environnementales
+full_clean <- full_data %>%
+  rename(
+    sel = `sel (‰)`,
+    O2_T0 = `dioxygène (T0) (mg/L)`,
+    O2_T4 = `dioxygène (T4) (mg/L)`,
+    DBO4 = `DBO4 (mg/L)`,
+    Temp = `T° (°C)`,
+    cond = `conductivité (µS/cm)`,
+    MES = `matière en suspension (mg/L)`
+  )
+
+# 2. Calcul des corrélations (Spearman)
+cor_results <- full_clean %>%
+  group_by(genus) %>%
+  summarise(
+    cor_pH = cor(abondance, pH, method = "spearman", use = "complete.obs"),
+    cor_sel = cor(abondance, sel, method = "spearman", use = "complete.obs"),
+    cor_O2 = cor(abondance, O2_T0, method = "spearman", use = "complete.obs"),
+    cor_DBO4 = cor(abondance, DBO4, method = "spearman", use = "complete.obs"),
+    cor_temp = cor(abondance, Temp, method = "spearman", use = "complete.obs"),
+    cor_cond = cor(abondance, cond, method = "spearman", use = "complete.obs"),
+    cor_MES = cor(abondance, MES, method = "spearman", use = "complete.obs")
+  )
+
+cor_strong <- cor_long %>%
+  filter(abs(correlation) > 0.6)
+
+
+cor_stronger <- cor_long %>%
+  filter(abs(correlation) > 0.8)
+
+plot(cor_stronger)
+
+ggplot(cor_stronger, aes(x = correlation, y = genus)) +
+  geom_col() +
+  facet_wrap(~variable, scales = "free") +
+  theme_minimal()
